3f625e2b4ab33be0caa28c0e67ee5dbde8b67f7a
Steve Pletcher <spletche@broadinstitute.org>
2019-07-12 11:59:11 -0400

    get rid of polymorphism traces

--- app/models/mailing_list_email.rb
+++ app/models/mailing_list_email.rb
@@ -10,15 +10,11 @@ class MailingListEmail < ApplicationRecord
 
   def self.no_decline_rsvp_for_event(event)
     left_joins(:user).where.not(
-      # not bothering to specify invitable_type and attendable_type because i plan to
-      # refactor out the polymorphism shortly and there's only one of each right now.
-      # also, directly interpolating parameters because there's an arel glitch with
-      # parameterized `where` queries + `exists`
-      Attendance.where("invitable_id = users.id and attendable_id = #{event.id} and rsvp_status = 'No'").exists
+      Attendance.where("user_id = users.id and event_id = #{event.id} and rsvp_status = 'No'").exists
     )
   end
 
   def self.attending_event(event)
-    joins(user: :attendances).where(attendances: {attendable_id: event.id, rsvp_status: ['Yes', 'Maybe']})
+    joins(user: :attendances).where(attendances: {event_id: event.id, rsvp_status: ['Yes', 'Maybe']})
   end
 end
