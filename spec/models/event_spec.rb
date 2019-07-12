43fdc1a3f774f2245f3379ec3abd255bce579e9d
Steve Pletcher <spletche@broadinstitute.org>
2019-07-12 11:47:35 -0400

    Fix timestamp parsing and display

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,7 +2,20 @@ require 'rails_helper'
 
 describe Event do
 
-  it "lets you save a valid event"
-  it "rejects events that start after they end"
+  it "lets you save a valid event" do
+    huge_party = Event.new(start_time: "05/01/2019 5:05 pm", end_time: "05/01/2019 5:07 pm")
+    expect(huge_party).to be_valid
+  end
+  it "rejects events that start after they end" do
+    time_travel_convention = Event.new(start_time: Time.current, end_time: Time.current - 1)
+    expect(time_travel_convention).not_to be_valid
+    expect(time_travel_convention.errors.messages.keys).to include :end_time
+  end
+  it "parses timestamps properly" do
+    huge_party = Event.new(start_time: "05/01/2019 5:05 pm", end_time: "05/01/2019 5:07 pm")
+    expect(huge_party).to be_valid
+    expect(huge_party.start_time).to eq Time.new(2019, 5, 1, 17, 5, 0)
+    expect(huge_party.end_time).to eq Time.new(2019, 5, 1, 17, 7, 0)
+  end
 
 end
