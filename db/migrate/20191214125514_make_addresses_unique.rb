b6b8766f77bd334d313055211cfd92df0cf268c8
Steve Pletcher <spletche@broadinstitute.org>
2019-12-28 18:43:19 -0500

    find block

--- db/migrate/20191214125514_make_addresses_unique.rb
+++ db/migrate/20191214125514_make_addresses_unique.rb
@@ -2,7 +2,7 @@ class MakeAddressesUnique < ActiveRecord::Migration[5.2]
   def change
     previously_seen_addresses = []
     Address.all.each do |address|
-      if already_seen = previously_seen_addresses.find(address)
+      if already_seen = previously_seen_addresses.find{|x| x == address }
         # update any events referencing this address to the earliest instance of it
         Event.where(address: address).update_all(address_id: already_seen.id)
         address.destroy!
