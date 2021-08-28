841e4f60dfd227e59e89a9260559e63f68cdcadf
Steve Pletcher <4468322+ThePletch@users.noreply.github.com>
2021-08-28 18:14:41 -0400

    Handle some formatting problems with gcal link

--- app/helpers/gcal_helper.rb
+++ app/helpers/gcal_helper.rb
@@ -5,9 +5,9 @@ module GcalHelper
           end_datetime: event.end_datetime,
           title: event.title,
           timezone: ActiveSupport::TimeZone::MAPPING[Time.zone.name],
-          location: event.address.to_s,
+          location: event.address.to_s.gsub("\n", " "),
           url: event_url(event),
-          description: event.description
+          description: event.description.gsub(/\n+/, "\n")
         ).google_url
     end
 end
