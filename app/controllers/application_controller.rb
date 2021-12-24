ba4f57baf9f9e693b578bb9e62eec00c55bb05de
Steve Pletcher <4468322+ThePletch@users.noreply.github.com>
2021-12-24 10:28:18 -0500

    Add locale routing, fix validation error rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :permit_additional_user_params, if: :devise_controller?
+  before_action :set_locale
 
   private
+
+  def set_locale
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale =  params[:locale]
+      else
+        raise I18n::InvalidLocale.new(params[:locale])
+      end
+    else
+      I18n.locale = I18n.default_locale
+    end
+  end
 
   def permit_additional_user_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
-  mount Commontator::Engine => '/comments_api'
+  scope "(:locale)" do
+    devise_for :users
+    mount Commontator::Engine => '/comments_api'
 
-  resources :events, shallow: true do
-    member do
-      get 'ical'
+    resources :events, shallow: true do
+      member do
+        get 'ical'
+      end
+
+      resources :attendances, only: [:create, :update, :destroy]
+      resources :polls, except: [:index, :show], shallow: true do
+        resources :poll_responses, only: [:create, :update, :destroy]
+      end
     end
 
-    resources :attendances, only: [:create, :update, :destroy]
-    resources :polls, except: [:index, :show], shallow: true do
-      resources :poll_responses, only: [:create, :update, :destroy]
+    # user-scoped events index
+    resources :user, shallow: true, only: [] do
+      resources :events, only: [:index]
     end
-  end
 
-  # user-scoped events index
-  resources :user, shallow: true, only: [] do
-    resources :events, only: [:index]
-  end
-
-  resources :mailing_lists do
-    member do
-      put 'sync_users'
+    resources :mailing_lists do
+      member do
+        put 'sync_users'
+      end
     end
-  end
 
-  root "home#index"
+    root "home#index"
+  end
 end
