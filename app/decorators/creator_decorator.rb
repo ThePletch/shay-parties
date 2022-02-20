620bdec3148d7f70e5e09c0932062cf9a991bb9c
Steve Pletcher <4468322+ThePletch@users.noreply.github.com>
2022-02-20 16:10:14 -0500

    fix gravatars in attendee list

--- app/decorators/creator_decorator.rb
+++ app/decorators/creator_decorator.rb
@@ -1,10 +1,10 @@
 class CreatorDecorator < Draper::Decorator
   delegate :name
 
-  def gravatar_url
+  def gravatar_url(size: 60)
     # 60px avatars with the identicon style for people without a custom gravatar
     # todo config settings for this
-    options = {s: 60, d: 'identicon'}
+    options = {s: size, d: 'identicon'}
     "https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(object.email)}?#{options.to_query}"
   end
 end
