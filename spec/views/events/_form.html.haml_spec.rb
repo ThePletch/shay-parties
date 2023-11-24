9fd9d66627d10f57a5d23098d5865089f7d6c109
Shay Pletcher <4468322+ThePletch@users.noreply.github.com>
2023-11-24 13:40:21 -0500

    More tests

--- spec/views/events/_form.html.haml_spec.rb
+++ spec/views/events/_form.html.haml_spec.rb
@@ -1,6 +1,36 @@
 require "rails_helper"
 
-RSpec.describe "events/form" do
-  it "shows error messages if present"
-  it "asks if the event should be secret with proper explanation text"
+RSpec.describe "events/_form" do
+  before do
+    assign(:prior_addresses, [])
+  end
+
+  it "shows error messages if present" do
+    event = FactoryBot.build(
+      :event,
+      start_time: Time.current,
+      end_time: Time.current - 1,
+    )
+    event.valid?
+
+    render(
+      'events/form',
+      event: event
+    )
+
+    expect(rendered).to have_selector('.invalid-feedback')
+    expect(rendered).to have_selector('#error_explanation')
+  end
+
+  it "asks if the event should be secret with proper explanation text" do
+    event = FactoryBot.create(:event)
+
+    render(
+      'events/form',
+      event: event
+    )
+
+    expect(rendered).to have_text('Make this event secret')
+    expect(rendered).to have_text("Secret events don't appear on the home page unless someone has RSVPed")
+  end
 end
