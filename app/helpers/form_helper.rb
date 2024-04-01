4cff87668dbc3e27003d8c825109d978f010fd65
Shay Pletcher <4468322+ThePletch@users.noreply.github.com>
2024-04-01 08:45:53 -0400

    Properly pass in address attributes in event form

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -4,7 +4,11 @@ module FormHelper
   def optional_parent_wrap(parent_form, form_record, options = {}, &block)
     if parent_form.present?
       record_object = form_record.kind_of?(Array) ? form_record.last : form_record
-      parent_form.fields_for(record_object, options, &block)
+      if options[:record_name]
+        parent_form.fields_for(options[:record_name], record_object, options, &block)
+      else
+        parent_form.fields_for(record_object, options, &block)
+      end
     else
       bootstrap_form_for(form_record, options, &block)
     end
