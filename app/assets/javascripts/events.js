da375734d40af208ab739103a93328c9087ee9a0
Shay Pletcher <4468322+ThePletch@users.noreply.github.com>
2025-03-10 09:55:58 -0400

    Addresses: Ensure address is properly persisted in event edit form

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,6 +2,45 @@
 //= require jquery-ui/widgets/slider
 //= require jquery-ui-timepicker-addon
 
+const addressAttributeToFormFieldMap = {
+  'street': "#event_address_attributes_street",
+  'street2': "#event_address_attributes_street2",
+  'city': "#event_address_attributes_city",
+  'state': "#event_address_attributes_state",
+  'zip_code': "#event_address_attributes_zip_code"
+};
+
+async function updateAddressProperties(addressId) {
+  const response = await $.get({
+    url: `/addresses/${addressId}/`,
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  Object.keys(addressAttributeToFormFieldMap).forEach(function(key) {
+    const correspondingField = $(addressAttributeToFormFieldMap[key]);
+    correspondingField.val(response[key]);
+    correspondingField.prop('disabled', true);
+  });
+}
+
+function clearAddressProperties() {
+  Object.keys(addressAttributeToFormFieldMap).forEach(function(key) {
+    const correspondingField = $(addressAttributeToFormFieldMap[key]);
+    correspondingField.prop('disabled', false);
+  });
+}
+
+function handleAddressChange() {
+  const selectedId = $("#event_address_id").val();
+  if (selectedId === "") {
+    clearAddressProperties();
+  } else {
+    void updateAddressProperties(selectedId);
+  }
+}
+
 $(function() {
   $('.datetimepicker').datetimepicker({
     timeText: '',
@@ -9,36 +48,8 @@ $(function() {
     minuteGrid: 15,
   });
 
-  var addressAttributeToFormFieldMap = {
-    'street': "#event_address_attributes_street",
-    'street2': "#event_address_attributes_street2",
-    'city': "#event_address_attributes_city",
-    'state': "#event_address_attributes_state",
-    'zip_code': "#event_address_attributes_zip_code"
-  };
-
-  $("#prior_addresses").change(function(e) {
-    var selectedAddress = $(this).children('option:selected');
-    var idField = $("#existing_address_id");
-    if ([null, ""].includes(selectedAddress.val())) {
-      idField.val(null);
-
-      Object.keys(addressAttributeToFormFieldMap).forEach(function(key) {
-        var correspondingField = $(addressAttributeToFormFieldMap[key]);
-        correspondingField.val("");
-        correspondingField.prop('disabled', false);
-      });
-    } else {
-      // use the selected address's fields
-      idField.val(selectedAddress.val());
-
-      // fill in and lock all the 'new address' fields, since we're using an existing address.
-      // disabling the fields prevents them from being submitted with the form.
-      Object.keys(addressAttributeToFormFieldMap).forEach(function(key) {
-        var correspondingField = $(addressAttributeToFormFieldMap[key]);
-        correspondingField.val(selectedAddress.attr(key));
-        correspondingField.prop('disabled', true);
-      });
-    }
-  });
+  if ($("#event_address_id").length > 0) {
+    $("#event_address_id").change(handleAddressChange);
+    handleAddressChange();
+  }
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_24_194655) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_26_133426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -164,6 +164,27 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_24_194655) do
     t.index ["user_id"], name: "index_polls_on_user_id"
   end
 
+  create_table "signup_contexts", force: :cascade do |t|
+    t.boolean "guest_enterable"
+    t.bigint "event_id"
+    t.text "instructions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_signup_contexts_on_event_id"
+  end
+
+  create_table "signup_sheet_items", force: :cascade do |t|
+    t.bigint "event_id"
+    t.string "attendee_type"
+    t.bigint "attendee_id"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "requested", default: false
+    t.index ["attendee_type", "attendee_id"], name: "index_signup_sheet_items_on_attendee"
+    t.index ["event_id"], name: "index_signup_sheet_items_on_event_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -205,4 +226,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_24_194655) do
   add_foreign_key "comments", "comments", column: "parent_id"
   add_foreign_key "events", "addresses"
   add_foreign_key "events", "users"
+  add_foreign_key "signup_contexts", "events"
+  add_foreign_key "signup_sheet_items", "events"
 end
