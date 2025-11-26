2c8c71a397c01dca7090949ab6fb08ad276990d0
Shay Pletcher <me@shay.zone>
2025-11-26 17:10:43 -0500

    remove excess tables

--- db/schema.rb
+++ db/schema.rb
@@ -165,27 +165,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_27_234627) do
     t.index ["user_id"], name: "index_polls_on_user_id"
   end
 
-  create_table "signup_contexts", force: :cascade do |t|
-    t.boolean "guest_enterable"
-    t.bigint "event_id"
-    t.text "instructions"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_signup_contexts_on_event_id"
-  end
-
-  create_table "signup_sheet_items", force: :cascade do |t|
-    t.bigint "event_id"
-    t.string "attendee_type"
-    t.bigint "attendee_id"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "requested", default: false
-    t.index ["attendee_type", "attendee_id"], name: "index_signup_sheet_items_on_attendee"
-    t.index ["event_id"], name: "index_signup_sheet_items_on_event_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
